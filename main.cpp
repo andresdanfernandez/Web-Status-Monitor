faff6025391afe99dd75aff51f2566c23d8f518b
andresdanfernandez <46586316+andresdanfernandez@users.noreply.github.com>
2025-01-29 13:06:01 -0500

    create socket

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sys/socket.h>
 
 int main(int argc, char* argv[]) {
 
     if (argc != 2) {
         std::cerr << "Error incorrect arguement count\n";
         return 1;
+    }
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+    std::cerr << "Error creating socket\n";
+    return -1;
     }
 
     std::string filename = argv[1];
@@ -19,6 +27,10 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // if protocol is http, web server listening at port 80.
+    // else if it is https, web server listening at port 443.
+    // in the url example "fiu.gov" is the host name which must be translated to an IP address.
+
     std::cout << "File Opened!\n";
 
     std::string url;
@@ -29,9 +41,12 @@ int main(int argc, char* argv[]) {
         urls.push_back(url);
     }
 
-    for(std::string url : urls) {
-        std::cout << url << '\n';
-    }
+    // start with one url -> http://fiu.gov
+    std::string firstUrl = urls[0];
+
+
+
+
 
     file.close();
     return 0;
